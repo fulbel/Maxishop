069c07a063de3cd520cd3d015ea5e2c90bc8568b
mscholz-tgm <43068592+mscholz-tgm@users.noreply.github.com>
2019-03-10 17:38:27 +0100

    Add files via upload

---
+++ maxishop/shop.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS shop;
+CREATE DATABASE shop;
+USE shop;
+
+CREATE TABLE items(
+  name VARCHAR(20),
+  kosten INTEGER,
+  beschreibung VARCHAR(100),
+  gewicht INTEGER,
+  menge INTEGER,
+  imgsrc VARCHAR(20),
+  PRIMARY KEY(name)
+)ENGINE=INNODB;
+
+CREATE TABLE user (
+  uname VARCHAR(20),
+  vname VARCHAR(20),
+  nname VARCHAR(20),
+  email VARCHAR(100),
+  pw VARCHAR(20),
+  isAdmin BOOLEAN,
+  PRIMARY KEY (uname)
+)ENGINE=INNODB;
+
+GRANT ALL ON shop.* TO 'insy3'@'localhost';
