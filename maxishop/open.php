d9f69cbe67c83242327295f268a78c50c8246178
mscholz-tgm <43068592+mscholz-tgm@users.noreply.github.com>
2019-04-24 19:52:51 +0200

    Add files via upload

--- maxishop/open.php
+++ maxishop/open.php
@@ -4,6 +4,8 @@ $pdo = new PDO('mysql:host=localhost;dbname=shop','root','');
 $additem = $pdo -> prepare(
   "INSERT INTO item (name,kosten,beschreibung,gewicht,menge,imgsrc)
    VALUES (?,?,?,?,?,?)");
+$deleteitem = $pdo -> prepare("DELETE FROM item WHERE name = ?");
+
 $editname = $pdo -> prepare("UPDATE item name=? WHERE name=?");
 $editkosten = $pdo -> prepare("UPDATE item kosten=? WHERE name=?");
 $editbeschreibung = $pdo -> prepare("UPDATE item beschreibung=? WHERE name=?");
