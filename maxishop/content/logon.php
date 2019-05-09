721abee7d1119e8f8dc301568595dde118a069b5
Fridolin Ulbel <33911633+fulbel@users.noreply.github.com>
2019-05-09 09:48:25 +0200

    updated to database

--- maxishop/content/logon.php
+++ maxishop/content/logon.php
@@ -1,17 +1,15 @@
-<?php $bname = $_POST [ "bname" ] ;
-$password = md5($_POST [ "password" ]);
-$users = file ( "users.txt" ) ;
-foreach ( $users AS $line ){
-  $user_info = explode ( ";" , $line ) ;
-if($user_info [ 2 ] == $bname AND $user_info [ 1 ] == $password){
-  $_SESSION['a_user'] = $bname;
+<?php 
+$uname = $_POST ["bname"];
+$pw = md5($_POST ["password"]);
+$user = $pdo -> query("SELECT (uname,pw) FROM USER WHERE uname=".$uname." AND pw=".$pw)-> fetch();
+if(isset($user['uname'])){
+	$_SESSION['a_user'] = $bname;
   header("Location: index.php");
   if(isset($_SESSION['message'])){
     unset($_SESSION['message']);
   }
-  break;
 }else {
   $_SESSION['message'] = "<script type='text/javascript'>alert('Eingaben Falsch');</script>";
   header("Location: index.php");
 }
-} ?>
+?>
