64193eb95a5398f52b476f37c2a0e33219b2ec20
mstocker-tgm <32959152+mstocker-tgm@users.noreply.github.com>
2019-05-09 08:33:04 +0200

    Update cart.php

--- maxishop/content/cart.php
+++ maxishop/content/cart.php
@@ -16,18 +16,15 @@
       <tbody>
         <?php
         if(isset($_POST['select'])){
-        $warenkorb = $_POST['select'];
-          $keys = array_keys($warenkorb);
-          foreach ($keys AS $ware) {
-            foreach ($pdo->query("SELECT name,kosten,beschreibung,gewicht,menge,imgsrc FROM item") AS $item) {
-              if($ware == $item['name']){
-                echo "<tr><td><img src='../img/".$item['imgsrc']."' width='200px' height='200px'</td>";
-                echo "<td>".$item['name']."</td>";
-                echo "<td>".$item['beschreibung']."</td>";
-                echo "<td>".$item['gewicht']."</td>";
-                echo "<td>".$item['kosten']."</td>";
-                echo "<td>".$warenkorb[$ware]."</td>";
-              }
+          $warenkorb = $pdo->query("SELECT name,kosten,beschreibung,gewicht,menge,imgsrc FROM item");
+          foreach ($warenkorb AS $item) {
+            if($item['bestellt'] > 0){
+               echo "<tr><td><img src='../img/".$item['imgsrc']."' width='200px' height='200px'</td>";
+               echo "<td>".$item['name']."</td>";
+               echo "<td>".$item['beschreibung']."</td>";
+               echo "<td>".$item['gewicht']."</td>";
+               echo "<td>".$item['kosten']."</td>";
+               echo "<td>".$item['bestellt']."</td>";
             }
           }
         }
